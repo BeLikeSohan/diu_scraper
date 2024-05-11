3af19a8e48e8ca75fe3c0d96c0db5a8fcfc49efd
Washiul Alam Sohan <34248839+BeLikeSohan@users.noreply.github.com>
2024-05-12 02:48:13 +0600

    a

--- main.py
+++ main.py
@@ -13,33 +13,36 @@ link_queue = [start_url]
 
 def crawl():
     while True:
-        url = link_queue.pop(0)
-        print("Scrapping: {}".format(url))
+        try:
+            url = link_queue.pop(0)
+            print("Scrapping: {}".format(url))
 
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, features="html.parser")
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, features="html.parser")
 
-        temp = []
-        for link in soup.find_all('a', href=True):
-            link_url = link["href"]
-            if check_url(link_url):
-                if url not in link_queue:
-                    temp.append(link_url)
-        if len(temp) > 0:
-            for temp_url in temp:
-                link_queue.append(temp_url)
-            print("Added {} URLs in the queue, total: {}".format(len(temp), len(link_queue)))
+            temp = []
+            for link in soup.find_all('a', href=True):
+                link_url = link["href"]
+                if check_url(link_url):
+                    if url not in link_queue:
+                        temp.append(link_url)
+            if len(temp) > 0:
+                for temp_url in temp:
+                    link_queue.append(temp_url)
+                print("Added {} URLs in the queue, total: {}".format(len(temp), len(link_queue)))
 
-        markdown = md(response.content, strip=['a', 'img', 'nav'])
-        with open(os.path.join('data/md', sanitize_url(url)), "w", encoding="utf-8") as md_file:
-            markdown = re.sub(r'\n\s*\n', '\n\n', markdown)
-            md_file.write(markdown)
+            markdown = md(response.content, strip=['a', 'img', 'nav'])
+            with open(os.path.join('data/md', sanitize_url(url)), "w", encoding="utf-8") as md_file:
+                markdown = re.sub(r'\n\s*\n', '\n\n', markdown)
+                md_file.write(markdown)
 
-        print("URLs Left: {}".format(len(link_queue)))
+            print("URLs Left: {}".format(len(link_queue)))
 
-        if len(link_queue) == 0:
-            print("=== CRAWLING DONE ===")
-            break
+            if len(link_queue) == 0:
+                print("=== CRAWLING DONE ===")
+                break
+        except:
+            print("ERROR: Skipping...")
 
 
 if __name__ == "__main__":
